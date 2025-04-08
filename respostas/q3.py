39055e4e90e8d980976c1713c069faf97aba071e
melquezdk <104629528+melquezdk@users.noreply.github.com>
2025-04-08 20:59:09 -0300

    Update q3.py

--- respostas/q3.py
+++ respostas/q3.py
@@ -1,2 +1,2 @@
 #Crie um programa que exiba as seguintes informações, cada uma em uma linha:
-print ("Cristiano Ronaldo\nLicenciatura em Computação\nIFPI")
+print ("Melquesedeque\nLicenciatura em Computação\nIFPI")
